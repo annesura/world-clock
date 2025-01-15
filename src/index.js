c456dd79d0a5f5ca0bbab66a224fefd1ee565d68
annesura <annesura00@gmail.com>
2025-01-15 18:12:44 -0500

    Added Paris to display cities on main page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ let losAngelesTime = moment().tz("America/Los_Angeles");
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
 
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
 let sydneyElement = document.querySelector("#sydney");
 let sydneyDateElement = sydneyElement.querySelector(".date");
 let sydneyTimeElement = sydneyElement.querySelector(".time");
